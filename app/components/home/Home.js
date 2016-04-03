17e07d9f94acf6ebd1fd8faf3277b7274edf0d3b
nxy <nxy801111@hotmail.com>
2016-04-04 01:36:27 +0800

    首页分类点击列表

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -5,74 +5,82 @@
  *  Description: 首页
  */
 const MockData_banner = [
-  {id: '1', url: require('../../../assets/banner/1.png')},
-  {id: '2', url: require('../../../assets/banner/2.png')},
-  {id: '3', url: require('../../../assets/banner/3.png')},
-  {id: '4', url: require('../../../assets/banner/4.png')}
+  {id: '1', uri: require('../../../assets/banner/1.png')},
+  {id: '2', uri: require('../../../assets/banner/2.png')},
+  {id: '3', uri: require('../../../assets/banner/3.png')},
+  {id: '4', uri: require('../../../assets/banner/4.png')}
 ];
 const MockData_region1 = [
-  {id: '1', url: require('../../../assets/banner/1.png'), text: '桑拿, 蒸腾芬兰'},
-  {id: '2', url: require('../../../assets/banner/2.png'), text: '伦敦, 新西区时代'},
-  {id: '3', url: require('../../../assets/banner/3.png'), text: '马里, 西非DNA'},
-  {id: '4', url: require('../../../assets/banner/4.png'), text: '曼哈顿, 文艺彻骨'}
+  {id: '1', uri: require('../../../assets/banner/1.png'), text: '桑拿, 蒸腾芬兰'},
+  {id: '2', uri: require('../../../assets/banner/2.png'), text: '伦敦, 新西区时代'},
+  {id: '3', uri: require('../../../assets/banner/3.png'), text: '马里, 西非DNA'},
+  {id: '4', uri: require('../../../assets/banner/4.png'), text: '曼哈顿, 文艺彻骨'}
 ];
 const MockData_region2 = [
   {
-    id: '1', url: require('../../../assets/banner/1.png'),
+    id: '1', uri: require('../../../assets/banner/1.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '焦作',
-    price: '￥1999.0起'
+    price: '￥1999.0起',
+    category: '跟团游'
 
   },
   {
-    id: '2', url: require('../../../assets/banner/2.png'),
+    id: '2', uri: require('../../../assets/banner/2.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '深圳',
-    price: '￥599.0起'
+    price: '￥599.0起',
+    category: '自助游'
   },
   {
-    id: '3', url: require('../../../assets/banner/3.png'),
+    id: '3', uri: require('../../../assets/banner/3.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '大连',
-    price: '￥2999.0起'
+    price: '￥2999.0起',
+    category: '自助游'
   },
   {
-    id: '4', url: require('../../../assets/banner/4.png'),
+    id: '4', uri: require('../../../assets/banner/4.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '焦作',
-    price: '￥999.0起'
+    price: '￥999.0起',
+    category: '跟团游'
   },
   {
-    id: '5', url: require('../../../assets/banner/1.png'),
+    id: '5', uri: require('../../../assets/banner/1.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '郑州',
-    price: '￥1999.0起'
+    price: '￥1999.0起',
+    category: '自助游'
   },
   {
-    id: '6', url: require('../../../assets/banner/2.png'),
+    id: '6', uri: require('../../../assets/banner/2.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '北京',
-    price: '￥1999.0起'
+    price: '￥1999.0起',
+    category: '跟团游'
   },
   {
-    id: '7', url: require('../../../assets/banner/3.png'),
+    id: '7', uri: require('../../../assets/banner/3.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '珠海',
-    price: '￥699.0起'
+    price: '￥699.0起',
+    category: '跟团游'
   },
   {
-    id: '8', url: require('../../../assets/banner/4.png'),
+    id: '8', uri: require('../../../assets/banner/4.png'),
     title: `<曼谷+芭提雅6日跟团游>万人折服谁定大会/五族城堡/千年云石公园/地道美食一价全含`,
     text: '1.全国独家万人水灯祈福 | 2.浪漫夜游湄南河...',
     starting: '天津',
-    price: '￥3999.0起'
+    price: '￥3999.0起',
+    category: '自助游'
   }
 ];
 const MockData_ICON = [
@@ -111,6 +119,12 @@ const styles = StyleSheet.create(
       marginTop: 140,
       backgroundColor: 'transparent',
       color: 'white'
+    },
+    nestedText2: {
+      color: 'white',
+      fontSize: 10,
+      padding: 2,
+      width: 35
     },
   }
 );
@@ -179,7 +193,7 @@ export default class Home extends Component {
   renderRegion1(item) {
     return (
       <TouchableOpacity key={item.id} style={styles.region1} onPress={() => Actions.bannerDetail({data: item.text})}>
-        <Image style={{flex: 1, width: deviceWidth / 2 - 10}} source={item.url} resizeMode={'stretch'} >
+        <Image style={{flex: 1, width: deviceWidth / 2 - 10}} source={item.uri} resizeMode={'stretch'} >
           <Text style={styles.nestedText}>
             {item.text}
           </Text>
@@ -190,10 +204,16 @@ export default class Home extends Component {
 
   renderRegion2() {
     return MockData_region2.map((item) => {
+      let bgColor = {backgroundColor: 'green'};
+      if(item.category === '跟团游') {
+        bgColor = {backgroundColor: 'orange'}
+      }
       return (
         <TouchableOpacity key={item.id} onPress={() => Actions.bannerDetail({data: item.title})}
                           style={{height: 80, width: deviceWidth - 10, flexDirection: 'row', marginVertical:3}}>
-          <Image style={{width: 80, height: 80}} source={item.url}/>
+          <Image style={{width: 80, height: 80}} source={item.uri}>
+            <Text style={[styles.nestedText2, bgColor]}>{item.category}</Text>
+          </Image>
           <View style={{flex: 1, marginHorizontal: 10}}>
             <Text style={{fontWeight: '400', fontSize: 13}}>{item.title}</Text>
             <Text style={{fontWeight: '200', fontSize: 12}}>{item.text}</Text>
